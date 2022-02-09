eb22190592980832bbccf47974667b45c23843d5
articuno870 <abhishek.prasad871@gmail.com>
2022-02-09 23:32:21 +0530

    read properties file in test case

--- src/test/java/com/test/myproject/restwebservice/RestWebServiceApplicationTests.java
+++ src/test/java/com/test/myproject/restwebservice/RestWebServiceApplicationTests.java
@@ -1,13 +1,33 @@
 package com.test.myproject.restwebservice;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration(initializers = ConfigFileApplicationContextInitializer.class)
+@ActiveProfiles("test")
 class RestWebServiceApplicationTests {
 
+	@Autowired
+	Environment env;
+	
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void testFile() {
+		System.out.println(env.getProperty("user.name"));
+		System.out.println(env.getProperty("emp.name"));
+		System.out.println("");
 	}
 
 }
